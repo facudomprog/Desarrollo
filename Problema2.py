578f683ddfcd7bc80bc8443dc4372c54bf042a41
Facundo Dominguez <facudominguez05@gmail.com>
2024-09-11 20:45:52 -0300

    Problema 2 Resuelto

---
+++ Problema2.py
@@ -0,0 +1,14 @@
+
+nombre = str(input("Ingrese su nombre: "))
+sexo = str(input("Ingrese su sexo: "))
+
+if sexo == "F":
+    if nombre[0] < "M":
+        print(nombre, sexo, "Grupo A", sep=", ")
+    else:
+        print(nombre, sexo, "Grupo B", sep=", ")
+elif sexo == "M":
+    if nombre[0] > "N":
+        print(nombre, sexo, "Grupo A", sep=", ")
+    else:
+        print(nombre, sexo, "Grupo B", sep=", ")
